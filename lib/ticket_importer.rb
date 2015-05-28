7432131a2af426802786fb0a9e725ce2be98cf4b
soojeen <soojeen@gmail.com>
2015-05-28 17:39:55 +0000

    update ticket importer with new columns

--- lib/ticket_importer.rb
+++ lib/ticket_importer.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require './app/models/ticket.rb'
+require './app/models/location.rb'
 
 class TicketImporter
 
@@ -8,16 +9,21 @@ class TicketImporter
   end
 
   def import
-    puts "Importing tickets from '#{@filename}'"
+    field_names = ['date', 'code', 'category', 'cost', 'time', 'plates']
+    puts "Importing students from '#{@filename}'"
     failure_count = 0
     Ticket.transaction do
-      CSV.read(@filename).each do |line|
+      CSV.read(@filename).each do | line |
+        data = [line[1], line[2], line[3], line[4], line[5], line[-1]]
+        attribute_hash = Hash[field_names.zip(data)]
         begin
-          Ticket.create!(address: line[7])
+          Ticket.create!(attribute_hash)
           print '.'
         rescue ActiveRecord::UnknownAttributeError
           failure_count += 1
           print '!'
+        ensure
+          STDOUT.flush
         end
       end
     end
@@ -25,6 +31,56 @@ class TicketImporter
     puts "\nDONE #{failures}\n\n"
   end
 
+
+    # puts "Importing tickets from '#{@filename}'"
+    # failure_count = 0
+    # Ticket.transaction do
+    #   CSV.read(@filename).each do |line|
+    #     begin
+    #       Ticket.create!(address: line[7])
+    #       print '.'
+    #     rescue ActiveRecord::UnknownAttributeError
+    #       failure_count += 1
+    #       print '!'
+    #     end
+    #   end
+    # end
+    # failures = failure_count > 0 ? "(failed to create #{failure_count} student records)" : ''
+    # puts "\nDONE #{failures}\n\n"
+
 end
 
 
+
+
+
+# class StudentsImporter
+
+#   def initialize(filename=File.absolute_path('db/data/students.csv'))
+#     @filename = filename
+#   end
+
+#   def import
+#     field_names = ['first_name', 'last_name', 'gender', 'birthday', 'email', 'phone']
+#     puts "Importing students from '#{@filename}'"
+#     failure_count = 0
+#     Student.transaction do
+#       File.open(@filename).each do |line|
+#         data = line.chomp.split(',')
+#         attribute_hash = Hash[field_names.zip(data)]
+#         begin
+#           Student.create!(attribute_hash)
+#           print '.'
+#         rescue ActiveRecord::UnknownAttributeError
+#           failure_count += 1
+#           print '!'
+#         ensure
+#           STDOUT.flush
+#         end
+#       end
+#     end
+#     failures = failure_count > 0 ? "(failed to create #{failure_count} student records)" : ''
+#     puts "\nDONE #{failures}\n\n"
+#   end
+
+# end
