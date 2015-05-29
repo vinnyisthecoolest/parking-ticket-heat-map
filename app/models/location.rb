dba7144b8c6e748a3dfacfff9175716cf66ef271
vinnyisthecoolest <vinnyisthecoolest@gmail.com>
2015-05-29 12:48:44 -0400

    add validation for address, rescue validition error

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,7 @@
-require 'geocoder'
-
 class Location < ActiveRecord::Base
   extend ::Geocoder::Model::ActiveRecord
   has_many :tickets
+  validates :address, presence: true
   geocoded_by :full_street_address
 
   def full_street_address
--- lib/ticket_importer.rb
+++ lib/ticket_importer.rb
@@ -9,17 +9,25 @@ class TicketImporter
   end
 
   def import
-    field_names = ['date', 'code', 'category', 'cost', 'time', 'plates']
-    puts "Importing students from '#{@filename}'"
+    field_names = %w(date code category cost time plates)
+    puts "Importing tickets from '#{@filename}'"
+    failure_count  = 0
     Ticket.transaction do
       CSV.read(@filename).each do | line |
-        location = Location.find_or_create_by!(address: line[7])
+        begin
+          location = Location.find_or_create_by!(address: line[7])
+        rescue ActiveRecord::RecordInvalid
+          failure_count += 1
+        ensure
+          STDOUT.flush
+        end
         data = [line[1], line[2], line[3], line[4], line[5], line[-1]]
         attribute_hash = Hash[field_names.zip(data)]
-        location.tickets.create!(attribute_hash)
+        location.tickets.create!(attribute_hash) if location
       end
     end
-    puts "\nDONE\n\n"
+    failures = failure_count > 0 ? "(failed to create #{failure_count} tickets)" : ''
+    puts "\nDONE #{failures}\n\n"
   end
 
 end
