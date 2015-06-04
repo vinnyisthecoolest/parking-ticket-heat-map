6578439b3949fc24640c93ee6de09e3a43b24698
soojeen <soojeen@gmail.com>
2015-06-04 21:42:18 +0000

    make adjustments for deployment

--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,7 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/contrib/all' # Requires cookies, among other things
 
-require 'pry'
+require 'pry' if development?
 require 'geocoder'
 
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
